bb5ba473b42f4cca36aa710598d7e23693c611a9
Paulo Vieira <80427803+vieirajunior-90@users.noreply.github.com>
2023-11-21 21:32:59 -0300

    Iniciado o projeto de microsserviço de Clientes

---
+++ ms-clientes/src/main/java/io/github/paulovieirajr/msclientes/MsClientesApplication.java
@@ -0,0 +1,13 @@
+package io.github.paulovieirajr.msclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClientesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsClientesApplication.class, args);
+    }
+
+}
---
+++ ms-clientes/src/test/java/io/github/paulovieirajr/msclientes/MsClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.paulovieirajr.msclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
