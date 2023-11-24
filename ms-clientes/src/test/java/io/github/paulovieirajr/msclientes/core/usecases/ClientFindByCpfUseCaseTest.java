851bbceaf36db2d7e949be07490dd26a20255c45
Paulo Vieira <80427803+paulovieirajr@users.noreply.github.com>
2023-11-24 14:18:09 -0300

    Ajustado os testes unitários dos usecases

--- ms-clientes/src/test/java/io/github/paulovieirajr/msclientes/core/usecases/ClientFindByCpfUseCaseTest.java
+++ ms-clientes/src/test/java/io/github/paulovieirajr/msclientes/core/usecases/ClientFindByCpfUseCaseTest.java
@@ -1,59 +1,64 @@
 package io.github.paulovieirajr.msclientes.core.usecases;
 
 
-import io.github.paulovieirajr.msclientes.api.dto.ClientRequest;
 import io.github.paulovieirajr.msclientes.api.dto.ClientResponse;
 import io.github.paulovieirajr.msclientes.api.exception.BusinessException;
-import io.github.paulovieirajr.msclientes.core.application.usecases.ClientFindByCpfUseCase;
-import io.github.paulovieirajr.msclientes.core.application.usecases.ClientRegisterUseCase;
+import io.github.paulovieirajr.msclientes.core.application.repositories.ClientRepositoryGateway;
+import io.github.paulovieirajr.msclientes.core.application.usecases.ClientFindByCpfUseCaseHandler;
+import io.github.paulovieirajr.msclientes.infra.model.ClientModel;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
 class ClientFindByCpfUseCaseTest {
 
-    public static final String VALID_CPF_TO_SAVE = "56010962013";
-    public static final String VALID_CPF_NOT_SAVED_IN_DATABASE = "88158375073";
+    public static final String VALID_CPF = "56010962013";
     public static final String INVALID_CPF = "12345678901";
 
-    @Autowired
-    ClientFindByCpfUseCase clientFindByCpfUseCase;
+    @InjectMocks
+    private ClientFindByCpfUseCaseHandler clientFindByCpfUseCaseHandler;
 
-    @Autowired
-    ClientRegisterUseCase clientRegisterUseCase;
+    @Mock
+    private ClientRepositoryGateway clientRepositoryGateway;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @DisplayName("Should return client response when CPF is valid and client exists")
     @Test
     public void validCpfAndClientExists() {
-        saveClient();
-        ClientResponse response = clientFindByCpfUseCase.execute(VALID_CPF_TO_SAVE);
-        assertNotNull(response);
+        ClientModel clientModel = new ClientModel();
+        clientModel.setCpf(VALID_CPF);
+        when(clientRepositoryGateway.findByCpf(VALID_CPF)).thenReturn(Optional.of(clientModel));
+
+        ClientResponse clientResponse = clientFindByCpfUseCaseHandler.execute(VALID_CPF);
+
+        assertNotNull(clientResponse);
+        assertEquals(VALID_CPF, clientResponse.cpf());
     }
 
     @DisplayName("Should throw EntityNotFoundException when CPF is valid but client does not exist")
     @Test
     public void validCpfAndClientDoesNotExist() {
-        assertThrows(EntityNotFoundException.class, () -> clientFindByCpfUseCase.execute(VALID_CPF_NOT_SAVED_IN_DATABASE));
+        when(clientRepositoryGateway.findByCpf(VALID_CPF)).thenReturn(Optional.empty());
+
+        assertThrows(EntityNotFoundException.class, () -> clientFindByCpfUseCaseHandler.execute(VALID_CPF));
     }
 
     @DisplayName("Should throw BusinessException when CPF is invalid")
     @Test
     public void invalidCpf() {
-        assertThrows(BusinessException.class, () -> clientFindByCpfUseCase.execute(INVALID_CPF));
-    }
-
-    private void saveClient() {
-        var clientRequest = new ClientRequest("John Doe", VALID_CPF_TO_SAVE, 30);
-        clientRegisterUseCase.execute(clientRequest);
+        assertThrows(BusinessException.class, () -> clientFindByCpfUseCaseHandler.execute(INVALID_CPF));
     }
 }
--- ms-clientes/src/test/java/io/github/paulovieirajr/msclientes/core/usecases/ClientRegisterUseCaseTest.java
+++ ms-clientes/src/test/java/io/github/paulovieirajr/msclientes/core/usecases/ClientRegisterUseCaseTest.java
@@ -2,28 +2,67 @@ package io.github.paulovieirajr.msclientes.core.usecases;
 
 
 import io.github.paulovieirajr.msclientes.api.dto.ClientRequest;
-import io.github.paulovieirajr.msclientes.core.application.usecases.ClientRegisterUseCase;
+import io.github.paulovieirajr.msclientes.api.dto.ClientResponse;
+import io.github.paulovieirajr.msclientes.api.exception.BusinessException;
+import io.github.paulovieirajr.msclientes.core.application.repositories.ClientRepositoryGateway;
+import io.github.paulovieirajr.msclientes.core.application.usecases.ClientRegisterUseCaseHandler;
+import io.github.paulovieirajr.msclientes.core.domain.entities.Client;
+import io.github.paulovieirajr.msclientes.core.domain.exception.ClientException;
+import io.github.paulovieirajr.msclientes.infra.model.ClientModel;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
 class ClientRegisterUseCaseTest {
 
-    @Autowired
-    ClientRegisterUseCase clientRegisterUseCase;
+    public static final long ID = 1L;
+    public static final String CLIENT_NAME = "John Doe";
+    public static final String VALID_CPF = "56010962013";
+    public static final int CLIENT_AGE = 30;
+    public static final String INVALID_CPF = "12345678901";
 
+    @InjectMocks
+    private ClientRegisterUseCaseHandler clientRegisterUseCaseHandler;
+
+    @Mock
+    private ClientRepositoryGateway clientRepositoryGateway;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @DisplayName("Should return client response when client registration is successful")
     @Test
-    @DisplayName("Should return a a client response when client is created")
-    void createClient() {
-        var clientRequest = new ClientRequest("John Doe", "56010962013", 30);
-        var clientResponse = clientRegisterUseCase.execute(clientRequest);
-        assertNotNull(clientResponse);
+    public void successfulClientRegistration() {
+        ClientRequest clientRequest = new ClientRequest(CLIENT_NAME, VALID_CPF, CLIENT_AGE);
+        ClientModel clientModel = new ClientModel(CLIENT_NAME, VALID_CPF, CLIENT_AGE);
+        clientModel.setId(ID);
+
+        when(clientRepositoryGateway.save(any(Client.class))).thenReturn(clientModel);
+
+        ClientResponse clientResponse = clientRegisterUseCaseHandler.execute(clientRequest);
+
+        assertEquals(ID, clientResponse.id());
+        assertEquals(CLIENT_NAME, clientResponse.name());
+        assertEquals(VALID_CPF, clientResponse.cpf());
+        assertEquals(CLIENT_AGE, clientResponse.age());
+    }
+
+    @DisplayName("Should throw BusinessException when client registration fails")
+    @Test
+    public void failedClientRegistration() {
+        ClientRequest clientRequest = new ClientRequest(CLIENT_NAME, INVALID_CPF, CLIENT_AGE);
+        when(clientRepositoryGateway.save(any(Client.class))).thenThrow(new ClientException("CPF Inválido"));
+
+        assertThrows(ClientException.class, () -> clientRegisterUseCaseHandler.execute(clientRequest));
     }
 }
