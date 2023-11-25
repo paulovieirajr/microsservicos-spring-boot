b588702a1a6d45153d6c422a9e702e4b8dc6f561
Paulo Vieira <paulovieiradev@outlook.com>
2023-11-25 01:05:13 -0300

    Configurado o gateway para receber requisições

--- ms-cloud-gateway/src/main/java/io/github/paulovieirajr/mscloudgateway/MsCloudGatewayApplication.java
+++ ms-cloud-gateway/src/main/java/io/github/paulovieirajr/mscloudgateway/MsCloudGatewayApplication.java
@@ -3,13 +3,22 @@ package io.github.paulovieirajr.mscloudgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MsCloudGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsCloudGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsCloudGatewayApplication.class, args);
+    }
+
+//	public RouteLocator routes(RouteLocatorBuilder routeLocatorBuilder) {
+//		return routeLocatorBuilder
+//				.routes()
+//					.route(r -> r.path("clients/**").uri("lb://ms-clientes"))
+//				.build();
+//	}
 
 }
