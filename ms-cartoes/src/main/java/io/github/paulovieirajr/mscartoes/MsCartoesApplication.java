d859fb81b86b6ccd1ad137b43e3929767647d825
Paulo Vieira <paulovieiradev@outlook.com>
2023-11-25 22:01:34 -0300

    Criado o projeto ms-cartoes

---
+++ ms-cartoes/src/main/java/io/github/paulovieirajr/mscartoes/MsCartoesApplication.java
@@ -0,0 +1,13 @@
+package io.github.paulovieirajr.mscartoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCartoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCartoesApplication.class, args);
+    }
+
+}
