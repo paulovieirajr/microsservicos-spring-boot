a87934ebe0ee6a76432d685a70b851bf3113083e
Paulo Vieira <paulovieiradev@outlook.com>
2023-11-25 22:06:14 -0300

    Habilitado a descoberta pelo Eureka Server

--- ms-cartoes/src/main/java/io/github/paulovieirajr/mscartoes/MsCartoesApplication.java
+++ ms-cartoes/src/main/java/io/github/paulovieirajr/mscartoes/MsCartoesApplication.java
@@ -2,8 +2,10 @@ package io.github.paulovieirajr.mscartoes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsCartoesApplication {
 
     public static void main(String[] args) {
