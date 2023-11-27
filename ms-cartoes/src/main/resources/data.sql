a315f2a1484fa77a3d69d1bfb1bf924318b79f24
Paulo Vieira <paulovieiradev@outlook.com>
2023-11-27 20:41:50 -0300

    Criado scripts para popular o banco

---
+++ ms-cartoes/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO credit_card (name, card_network, income, credit_limit)
+VALUES
+    ('Santander Visa Gold', 'VISA', 5000, 9500),
+    ('Bradesco Mastercard Silver', 'MASTERCARD', 3000, 5000),
+    ('Itau Visa Platinum', 'VISA', 10000, 15000),
+    ('Nubank Mastercard Black', 'MASTERCARD', 15000, 30000),
+    ('Santander Visa Infinite', 'VISA', 16000, 32000),
+    ('Itau American Express Basic', 'AMEX', 10000, 25000),
+    ('Itau American Express Gold', 'AMEX', 20000, 40000);
+
---
+++ ms-clientes/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO client (name, cpf, age)
+VALUES
+    ('Fernando Miguel', '76392510010', 30),
+    ('Mirella Silva', '28698308074', 25),
+    ('Lucia Camargo', '56152742048', 40),
+    ('Paulo Camargo', '32407503090', 42),
+    ('Alexandre Oliveira', '02522228064', 22);
